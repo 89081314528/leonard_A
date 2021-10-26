87ae3a507475ca311b0beaa195dce35df4789d45
julia <8908131428@mail.ru>
2021-10-26 19:08:22 +0300

    26/10

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,27 @@
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 1.1 Подсчет повторяющихся символов
+ */
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello word");
+        String str = "I like programming because it produces dopamine";
+        Map <Character, Integer> map = new HashMap();
+        for (int i = 0; i < str.length(); i++) {
+            Character key = str.charAt(i);
+            if (map.containsKey(key)) {
+                map.put(key, map.get(key) + 1);
+            } else {
+                map.put(key, 1);
+            }
+        }
+        for (Map.Entry<Character, Integer> entry : map.entrySet()) {
+            System.out.print(entry.getKey() + " " + entry.getValue() + " ");
+        }
+        // 2 вариант распечатки
+        System.out.println();
+        System.out.print(Arrays.toString(map.entrySet().toArray()));
     }
 }
